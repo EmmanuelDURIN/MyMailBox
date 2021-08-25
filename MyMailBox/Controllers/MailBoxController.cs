fbb4b33b377b859a780991d6bef6503e9003bd79
Emmanuel Durin <emmanueldurin@gmail.com>
2021-08-25 08:37:38 +0200

    Poco as model

---
+++ MyMailBox/Controllers/MailBoxController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMailBox.Models;
+using System;
+
+namespace MyMailBox.Controllers
+{
+  public class MailBoxController : Controller
+  {
+    public IActionResult Index(String reference)
+    {
+      var mailbox = new MailBox()
+      {
+        Reference = "X503",
+        Id = 1,
+        Color = "Red",
+        Depth = 200,
+        Width = 200,
+        Height = 200,
+        ImagePath = "images/mailboxes/mailbox1.jpg",
+        Name = "Ideal mailbox",
+      };
+      return View(model: mailbox);
+    }
+  }
+}
---
+++ MyMailBox/Models/MailBox.cs
@@ -0,0 +1,14 @@
+﻿namespace MyMailBox.Models
+{
+  public class MailBox
+  {
+    public int Id { get; set; }
+    public string Reference { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public string ImagePath { get; set; }
+    public double Height { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+  }
+}
